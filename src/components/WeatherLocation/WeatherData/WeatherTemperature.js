c4f31b6f9c0f56ebe7a4156854decb0dffe0e619
Diego Pastoriza <dpastoriza@cdt.com.ar>
2019-06-06 15:21:15 -0300

    seccion 2 (4) terminada

--- src/components/WeatherLocation/WeatherData/WeatherTemperature.js
+++ src/components/WeatherLocation/WeatherData/WeatherTemperature.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import WeatherIcons from 'react-weathericons';
+import PropTypes from 'prop-types';
+
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY
+} from './../../../constants/weathers';
 
 const icons = {
-    sunny:"day-sunny",
-    fog:"day-fog",
-
+    [CLOUD]: "cloud",
+    [CLOUDY]:"cloudy",
+    [SUN]:"day-sunny",
+    [RAIN]:"rain",
+    [SNOW]:"snow",
+    [WINDY]:"windy"
 };
 
 const getWeatherIcon = weatherState => {
@@ -24,5 +37,10 @@ const WeatherTemperature = ({temperature,weatherState}) => (
         <span>{temperature + "C°"}</span>
     </div>
 );
+
+WeatherTemperature.propTypes = {
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+};
 
 export default WeatherTemperature; 
