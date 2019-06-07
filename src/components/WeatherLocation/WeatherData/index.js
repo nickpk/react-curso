a15be1a75e5752f077277129ebd0b2443968900f
Diego Pastoriza <dpastoriza@cdt.com.ar>
2019-06-07 12:32:25 -0300

    se agrego proptype

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,21 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WeatherExtraInfo from './WeatherExtraInfo';
 import WeatherTemperature from './WeatherTemperature'
-import {
-        SUN
-} from './../../../constants/weathers';
 
 import './styles.css'
 
-const WeatherData = () => (
+const WeatherData = ({ data : {temperature,weatherState,humidity,wind} }) => (
     <div className="weatherDataCont">
-        <WeatherTemperature 
-            temperature={20} 
-            weatherState={SUN}/>
-        <WeatherExtraInfo 
-            humidity={80} 
-            wind={"10 m/s"}/>
-    </div>
-);
+            <WeatherTemperature 
+                temperature={temperature} 
+                weatherState={weatherState}/>
+            <WeatherExtraInfo 
+                humidity={humidity} 
+                wind={wind}/>
+    </div> 
+)
+
+WeatherData.PropTypes = {
+    data:PropTypes.shape({
+        temperature: PropTypes.number.isRequired,
+        weatherState:PropTypes.string.isRequired,
+        humidity: PropTypes.number.isRequired,
+        wind : PropTypes.string.isRequired,
+    })
+}
 
 export default WeatherData; 
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -4,11 +4,21 @@ import './styles.css';
 import Location from './Location';
 import WeatherData from './WeatherData';
 
+import {
+    SUN
+} from './../../constants/weathers';
+
+const data = {
+    temperature:5,
+    weatherState:SUN,
+    humidity:10,
+    wind: '10 m/s'
+}
 
 const WeatherLocation = () => (
     <div className="weatherLocationCont">
         <Location city={"Buenos Aires"}></Location>
-        <WeatherData></WeatherData>
+        <WeatherData data={data}></WeatherData>
     </div>
 );
     
