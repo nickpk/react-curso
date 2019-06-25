d4c26d60034956faada7743853c13187affe9963
Diego Pastoriza <dpastoriza9@gmail.com>
2019-06-25 01:33:09 -0300

    se agrega llamado a servidor para traer informacion mediante una promesa

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -5,21 +5,18 @@ import Location from './Location';
 import WeatherData from './WeatherData';
 
 import {
-    SUN,
-    WINDY
+    SUN
 } from './../../constants/weathers';
 
+const location = "Buenos Aires,ar";
+const api_key = "f99bbd9e4959b513e9bd0d7f7356b38d";
+const url_base_weather = "http://api.openweathermap.org/data/2.5/weather";
+const api_weather = `${url_base_weather}?q=${location}&appid=${api_key}`;
 const data = {
     temperature:5,
     weatherState:SUN,
     humidity:10,
     wind: '10 m/s'
-}
-const data2 = {
-    temperature:5,
-    weatherState:WINDY,
-    humidity:95,
-    wind: '40 m/s'
 }
 
 class WeatherLocation extends Component {
@@ -30,14 +27,34 @@ class WeatherLocation extends Component {
             data: data
         }
     }
+    getWeatherState = weather_data => {
+        return SUN;
+    }
+    getData = weather_data => {
+        const {humidity , temp } = weather_data.main;
+        const {speed} = weather_data.wind;
+        const weatherState = SUN;
+
+        const data = { 
+            humidity,
+            temperature : temp,
+            weatherState,
+            wind: `${speed} m/s`,
+        }
+    return data
+    }
     handlerUpdateClick = () =>{
-        console.log('Actualizando2');
-        this.setState({
-            city: "Colombia",
-            data : data2,
+        fetch(api_weather).then( resolve => {
+            return resolve.json();
+        }).then(data => {
+            const newWeather = this.getData(data);
+            console.log(newWeather);
+            console.log(data);
+            this.setState({
+                data: newWeather
+            })
         });
-        console.log(this.state.data);
-        debugger
+
     }
     render(){
         const {city, data} = this.state;
